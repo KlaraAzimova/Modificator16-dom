1bd81cba20c00fbeaf778baec537d82d79a13234
Klara <azimovaklara18@gmail.com>
2022-05-14 09:12:48 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Circle circle = new Circle();
+    circle.setPI(3.14);
+    circle.setRadius(5);
+    circle.toString();
+    final double a = Math.PI;
+        System.out.println("PI " + a);
+
+    }
+}
